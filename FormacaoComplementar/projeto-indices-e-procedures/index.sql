7236d3885f9d895a450b2ece795eced532583336
Thamyresarm <pink.thamyres@gmail.com>
2022-10-29 17:14:58 -0300

    atualizando sql

--- FormacaoComplementar/projeto-indices-e-procedures/index.sql
+++ FormacaoComplementar/projeto-indices-e-procedures/index.sql
@@ -1,13 +1,16 @@
 create index idx_btree_Rastreio on entrega(Codigo_Rastreio) using Btree;
 show index from ecommerce.entrega;
 
-select Codigo_Rastreio from entrega where StatusEntrega = 'Enviado';
+show index from ecommerce.pedido;
+
+select p.IdPedido, p.Descricao, e.StatusEntrega, Codigo_Rastreio from entrega e JOIN pedido p 
+WHERE p.IdPedido = e.IdPedido 
+AND StatusEntrega = 'Enviado';
 
 create index idx_btree_Produto on produto(Pnome) using Btree;
 show index from ecommerce.produto;
 
 create index idx_btree_Estoque on estoque(Localizacao) using Btree;
-
 show index from ecommerce.estoque;
 
 create index idx_btree_QtdEstoque on qtd_estoque(Qtd) using Btree;
@@ -16,4 +19,4 @@ show index from ecommerce.qtd_estoque;
 SELECT Pnome as Produto, Localizacao as Estoque, Qtd FROM qtd_estoque qe JOIN produto p , estoque e
 WHERE p.Idproduto = qe.IdProduto
 AND e.idEstoque = qe.IdEstoque
-AND Qtd > 10
+AND Qtd <= 20
